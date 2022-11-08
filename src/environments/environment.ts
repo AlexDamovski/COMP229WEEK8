160ae21b2abf2a0216776faeea3b01006e0562f9
AlexDamovski <112990760+AlexDamovski@users.noreply.github.com>
2022-11-07 22:21:04 -0500

    Week9 code completed (needs fixing on login)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'comp229-f2022-401ad.herokuapp.com/api'
 };
 
 /*
