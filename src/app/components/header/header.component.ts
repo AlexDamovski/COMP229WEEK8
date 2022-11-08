4df0e0a0cf15140e0d3d511d693496d7e8282ec7
AlexDamovski <112990760+AlexDamovski@users.noreply.github.com>
2022-11-07 21:15:04 -0500

    Static content pages to Angular

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isLoggedIn = false;
+  showMoviesList = false;
+  username? : string;
+
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  logout(): void {
+    
   }
 
 }
